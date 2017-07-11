d6eecac5e0a7506e5467d671bca469ea44e91b50
zhangliang <zhangliang>
2017-07-11 17:19:05 +0800

    back for time up

--- core/action.go
+++ core/action.go
@@ -53,3 +53,11 @@ func (p *ActionTypes) Contain(a ActionType) bool {
 	}
 	return false
 }
+
+type GangType int8
+
+const (
+	GT_Bu   GangType = iota + 1 // 补杠/扒杠
+	GT_An                       // 暗杠/自杠
+	GT_Dian                     // 点杠
+)
